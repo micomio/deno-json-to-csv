fd6958d2b7980ad5ba19c4acbafa57dcf323451f
micomio <michael.plefka@gmail.com>
2023-03-18 14:31:26 +0100

    Creates out.csv with parsed content. Added two wrong JSON files for testing purposes

--- src/jsontocsv.ts
+++ src/jsontocsv.ts
@@ -1,6 +1,10 @@
+import ITrack from "./interface/ITrack.ts";
+
 console.log(`++ JSON to csv ++`);
 
-const outFileName: string = "out.csv";
+const outFileName = "out.csv";
+const delimiter = ',';
+const debugEnabled = false;
 
 await readInputFile();
 
@@ -13,15 +17,37 @@ async function readInputFile() {
         console.log(`No input file found`);
         printHelpAndExit();
     } else {
-        console.log(`Arg for file found: ${inFileName}`);
+        debugEnabled && console.log(`Arg for file found: ${inFileName}`);
 
         await Deno.readTextFile(inFileName).then((file) => {
             console.log(`Successully read file with ${file.length} bytes.`);
+            const tracks: ITrack[] = parseJsonFile(file);
+            debugEnabled && tracks.forEach(entry => {
+                console.log(`Entry: ${JSON.stringify(entry)}`);
+            });
+
+            const csvContent: string = generateCsvTrackList(tracks);
+            Deno.writeTextFile(outFileName, csvContent);
+
         }).catch(() => {
             console.log(`Error reading file.`);
         });
-        
     }
+}
+
+function parseJsonFile(fileContent: string): ITrack[] {
+    return JSON.parse(fileContent);
+}
+
+function generateCsvTrackList(tracks: ITrack[]): string {
+    const header = Object.keys(tracks[0]);
+    const csv = [
+        header.join(delimiter),
+        ...tracks.map(entry => header.map(column => entry[column as keyof ITrack] ?? '').join(delimiter))
+    ].join('\r\n');   
+    
+    debugEnabled && console.log(`csv: ${csv}`);
+    return csv;
 }
 
 /* Helper */
